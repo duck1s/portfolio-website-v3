d3effe5823630193ff32463f1820015ea1fee2d1
Duck1s <joeri.breedveld123@gmail.com>
2022-06-16 18:06:18 +0200

    feat(experience): experience json, css and component

---
+++ components/Experience.tsx
@@ -0,0 +1,33 @@
+// Imports
+import styles from "../styles/components/Experience.module.scss"
+import Image from "next/image"
+
+// Icons
+import experience from "../public/assets/json/Experience.json"
+
+// Page
+export default function Experience() {
+	const experienceList = experience.map((job) => {
+		return (
+			<li key={job[0].id} className={styles.experience__listitem}>
+				<div className={styles.experience__img}>
+					<Image src={job[0].img} layout="fill" objectFit="cover"></Image>
+				</div>
+				<div className={styles.experience__text}>
+					<h4>{job[0].name}</h4>
+					<p>{job[0].description}</p>
+					<h4>{job[0].time}</h4>
+				</div>
+			</li>
+		)
+	})
+
+	return (
+		<>
+			<section className={styles.experience}>
+				<h3 className={styles.experience__heading}>Experiences</h3>
+				<ul className={styles.experience__list}>{experienceList}</ul>
+			</section>
+		</>
+	)
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,28 +2,24 @@
 import type { NextPage } from "next"
 import styles from "../styles/pages/About.module.scss"
 import Skills from "../components/Skills"
-import Image from "next/image"
+import Experience from "../components/Experience"
 
 // Page
 const About: NextPage = () => {
 	return (
 		<>
 			<section className={styles.about}>
-				<section className={styles.aboutleft}>
-					<section className={styles.aboutleft__content}>
-						<h2 className={styles.about__heading}>About</h2>
-						<p className={styles.about__text}>
+				<section className={styles.introduction}>
+					<h2 className={styles.about__heading}>About</h2>
+					<div className={styles.introduction__widget}>
+						<p className={styles.introduction__text}>
 							Hello, my name is Joeri Breedveld. I am a seventeen year old full-stack developer based in Amsterdam. I have been programming for one year. I am currently studying software
 							development at Mediacollege Amsterdam. I am very motivated to learn new things and I work every day to improve my coding skills.
 						</p>
-					</section>
-					<section className={styles.skills}>
-						<Skills />
-					</section>
+					</div>
 				</section>
-				<aside className={styles.aboutright}>
-					<Image className={styles.aboutaside} src={"/assets/undraw-about.svg"} width={500} height={400} />
-				</aside>
+				<Skills />
+				<Experience />
 			</section>
 		</>
 	)
